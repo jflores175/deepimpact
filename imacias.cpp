dcc79a54fd1f91b32c8545d92feff7c03564027b
Ivan Macias <imacias1@csub.edu>
2022-03-16 19:16:03 -0700

    merge test

--- imacias.cpp
+++ imacias.cpp
@@ -9,10 +9,11 @@
 
 #include <stdio.h>
 
-// This function will print out "Ivan Macias"
+// This function will print out message from this file
 void print_my_name() {
-	printf("Ivan Macias\n");
+	printf("From imacias.cpp\n");
+	printf("You pressed the f key\n"); // Tells what key was pressed
 }
 
-
-
+// This is to test merging branches
+// Will delete when merge complete
---
+++ imacias.h
@@ -0,0 +1,17 @@
+/*
+ *  Name: Ivan Macias
+ *  Date: Feb 24, 2022
+ *
+ *  @source: imacias.h
+ *
+ */
+
+#ifndef IMACIAS_H
+#define IMACIAS_H
+
+#include <stdio.h>
+
+// This function will print out message from this file
+void print_my_name();
+
+#endif
