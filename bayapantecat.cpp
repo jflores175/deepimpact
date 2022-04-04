50d7e0caf58fd08c7bee91d5c2cd42c1b95bfd82
Bryan Ayapantecatl <bayapantecat@odin.cs.csubak.edu>
2022-04-04 10:36:57 -0700

    Adding test to bayapantecat.cpp

--- bayapantecat.cpp
+++ bayapantecat.cpp
@@ -9,3 +9,13 @@ void show_name()
 {
     printf("Bryan\n");
 }
+
+void bayapantecat(int test)
+{
+    cout << "Please give me a positive nubmer: " << test << endl;
+
+    if (test > 0)
+        cout << "Congrats! It's positive!" << endl;
+    else
+        cout << "TEST FAILED!" << endl;
+}
