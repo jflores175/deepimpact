ee2ce5c4580ac844f291538457bc127c33e80301
Alexis Torres <alexistor583@gmail.com>
2022-04-14 15:23:37 -0700

    trying to implement a pause screen, added base functions to personal files

--- atorres.h
+++ atorres.h
@@ -5,14 +5,20 @@ class Menu
 {
 public:
 	bool display;
+	bool pause;
 	Menu();
 
 	// As of right now it should set the background for the menu
 	void show_menu(int red, int green, int blue, int xres, int yres);
+	
 	// This function will print out message from this file
 	void name();
+	
 	// Function to print out a string
 	void output(int xpos, int ypos, char* sent);
+	
+	// Function that should pause the game current game and show some options
+	void pause_screen(int red, int green, int blue, int xres, int yres);
 	~Menu();
 	
 };
