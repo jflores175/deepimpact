e4a46321489704ea98edb27df79a0bbba0cbf383
jflores <cs@pop-os.localdomain>
2022-04-24 21:09:24 -0700

    added functions to header

--- jflores.h
+++ jflores.h
@@ -10,6 +10,8 @@ void detect_edges_ship(Vec pos, int xres = 640, int yres= 480);
 void detect_edges_bullet(Vec pos, int xres = 640, int yres= 480);
 void jflores(int number);
 void check_arrow_keys(Vec pos, unsigned char keys[]);
-
+void reset_position(Vec pos);
+void change_position(Vec pos, int x, int y);
+void shift_position(Vec pos, int x, int y);
 
 #endif
